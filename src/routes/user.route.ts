ec9b1a6666c5397772a387cc667df855bcb8eaa9
Joshua K John <joshuakjohn7@gmail.com>
2024-10-17 23:35:56 +0530

    [joshua] add - added validation for signup and signin data

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,7 +1,9 @@
 import express, { IRouter } from 'express';
 import userController from '../controllers/user.controller';
+import UserValidator from '../validators/user.validator';
 
 class UserRoutes {
+  userValidator = new UserValidator();
   private UserController = new userController();
   private router = express.Router();
 
@@ -12,9 +14,10 @@ class UserRoutes {
   private routes = () => {
 
     //route to create a new user
-    this.router.post('/signup', this.UserController.newUser);
-
-    this.router.post('/signin', this.UserController.signIn)
+    this.router.post('/signup', this.userValidator.newUser, this.UserController.newUser)
+      
+    //route to signin
+    this.router.post('/signin', this.userValidator.signIn, this.UserController.signIn)
 
   };
 
--- src/validators/user.validator.ts
+++ src/validators/user.validator.ts
@@ -4,7 +4,22 @@ import { Request, Response, NextFunction } from 'express';
 class UserValidator {
   public newUser = (req: Request, res: Response, next: NextFunction): void => {
     const schema = Joi.object({
-      name: Joi.string().min(4).required()
+      fname: Joi.string().min(4).required(),
+      lname: Joi.string().min(1).required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().min(8).required()
+    });
+    const { error } = schema.validate(req.body);
+    if (error) {
+      next(error);
+    }
+    next();
+  };
+
+  public signIn = (req: Request, res: Response, next: NextFunction): void => {
+    const schema = Joi.object({
+      email: Joi.string().email().required(),
+      password: Joi.string().required()
     });
     const { error } = schema.validate(req.body);
     if (error) {
